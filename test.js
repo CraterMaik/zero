6bea97cf6de23221dbc2ee891b39a74f57f0c8d5
Crater <33011158+CraterMaik@users.noreply.github.com>
2019-06-27 21:09:17 -0500

    test

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 console.log('hello word');
-	let num = 12.3;
-	let perms = [1, 2, 3];
+let num = 12.3;
+let perms = [1, 2, 3];
 
-	let num1 = 1;
-	let num3 = 2;
-	let list = [0, 6, 8];
-	list.map(l => {
-		return l
-	})
+let num1 = 1;
+let num3 = 2;
+let list = [0, 6, 8];
+list.map(l => {
+	return l;
+})
